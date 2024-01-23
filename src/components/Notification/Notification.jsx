8bb7f5f66421a2ec171543d3f2b74b043b97984a
Yulia Zhukova <132227187+Zhyuli@users.noreply.github.com>
2024-01-23 22:37:08 +0200

    css

--- src/components/Notification/Notification.jsx
+++ src/components/Notification/Notification.jsx
@@ -2,7 +2,7 @@ import css from './Notification.module.css';
 
 export const Notification = () => {
   return (
-    <div>
+    <div className={css.box}>
       <p className={css.text}>No feedback yet.</p>
     </div>
   );
