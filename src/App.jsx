df20bbe872038c263fdf9d68fa8c69d0a7125b15
Yulia Zhukova <132227187+Zhyuli@users.noreply.github.com>
2024-01-23 12:31:23 +0200

    useState, useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,66 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import { Options } from './components/Options/Options';
 import { Feedback } from './components/Feedback/Feedback';
 import { Description } from './components/Description/Description';
 
-function App() {
-  const [feedbackCategory, setFeedbackCategory] = useState({
+const getFeedbacks = () => {
+  const savedFeedbacks = window.localStorage.getItem('saved-feedbacks');
+  if (savedFeedbacks !== null) {
+    return JSON.parse(savedFeedbacks);
+  }
+  return {
     good: 0,
     neutral: 0,
     bad: 0,
-  });
-  console.log(feedbackCategory);
-  console.log(setFeedbackCategory);
+  };
+};
+
+const getClicks = () => {
+  const savedClicks = window.localStorage.getItem('saved-clicks');
+  if (savedClicks !== null) {
+    return JSON.parse(savedClicks);
+  }
+  return 0;
+};
+
+export const App = () => {
+  const [feedbackCategory, setFeedbackCategory] = useState(getFeedbacks);
+  const [clicks, setClicks] = useState(getClicks);
+
+  // console.log(feedbackCategory);
+  // console.log(setFeedbackCategory);
+  // console.log(clicks);
+  // console.log(setClicks);
+
+  const onFeedback = option => {
+    setFeedbackCategory({
+      ...feedbackCategory,
+      [option]: feedbackCategory[option] + 1,
+    });
+    setClicks(clicks + 1);
+  };
+
+  const onReset = () => {
+    setFeedbackCategory({
+      ...feedbackCategory,
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    });
+    setClicks(0);
+  };
+
+  useEffect(() => {
+    window.localStorage.setItem('saved-feedbacks', JSON.stringify(feedbackCategory));
+    window.localStorage.setItem('saved-clicks', clicks);
+  }, [feedbackCategory, clicks]);
 
   return (
     <div>
       <Description />
-      <Options />
-      <Feedback />
+      <Options onUpdate={onFeedback} />
+      <Feedback feedbackCategory={feedbackCategory} />
     </div>
   );
-}
-
-export default App;
+};
--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,11 +1,21 @@
-export const Feedback = () => {
+export const Feedback = ({ feedbackCategory: { good, neutral, bad } }) => {
   return (
     <div>
-      <p>Good:</p>
-      <p>Neutral:</p>
-      <p>Bad:</p>
-      <p>Total:</p>
-      <p>Positive:</p>
+      <p>
+        Good: <span className="catigory">{good}</span>
+      </p>
+      <p>
+        Neutral: <span className="catigory">{neutral}</span>
+      </p>
+      <p>
+        Bad: <span className="catigory">{bad}</span>
+      </p>
+      <p>
+        Total: <span className="catigory">{good}</span>
+      </p>
+      <p>
+        Positive: <span className="catigory">{good}</span>
+      </p>
     </div>
   );
 };
